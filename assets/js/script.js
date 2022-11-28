a984a595704217e45a37f6e52824d6064c0fda87
Erickabarker <110427318+Erickabarker@users.noreply.github.com>
2022-11-28 20:09:18 +0000

    Player selection function defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,43 @@
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+const choicebutton = [rock, paper, scissors];
 
-let computerChoice = document.getElementById("computer-choice");
+
+let computerChoice = document.getElementById("computerChoice");
+let option = document.getElementById("choicebutton")
 
 
 function createComDecision(){
-    const choiceOptions = ['rock', 'paper', 'scissors'];
+    const selection = ['rock', 'paper', 'scissors'];
     const randomChoice = (Math.random()*3);
-    return choiceOptions[randomChoice];
+    return selection[randomChoice];
 }
 
 computerChoice = `Computers Choice ${createComDecision()}`;
 
+function play(playerSelection){
+ let playerChoice = document.getElementById("playerChoice");
+ playerChoice.innerHTML = `You Selected: $(playerChoice)`;
+
+ return playerSelection
+}
+
+
 scissors.addEventListener("click", function (){
-    var userChoice = document.getElementById("userChoice");
-    userChoice.innerHTML = "You chose scissors!"
+    var playerChoice = document.getElementById("playerChoice");
+    playerChoice.innerHTML = "You chose scissors!"
 
 })
 
 rock.addEventListener("click", function (){
-    var userChoice = document.getElementById("userChoice");
-    userChoice.innerHTML = "You chose Rock!"
+    var playerChoice = document.getElementById("playerChoice");
+    playerChoice.innerHTML = "You chose Rock!"
 
 })
 
 paper.addEventListener("click", function (){
-    var userChoice = document.getElementById("userChoice");
-    userChoice.innerHTML = "You chose Paper!"
+    var playerChoice = document.getElementById("playerChoice");
+    playerChoice.innerHTML = "You chose Paper!"
 
 })
