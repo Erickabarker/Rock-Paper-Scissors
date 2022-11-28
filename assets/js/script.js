50cd46abfa9a8080c4cda613e0f32e71b3d1b372
Erickabarker <110427318+Erickabarker@users.noreply.github.com>
2022-11-28 21:21:37 +0000

    altered function order for game bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,13 @@
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
-const choicebutton = [rock, paper, scissors];
+const choicebutton = ['rock', 'paper', 'scissors'];
 const playerScore = 0;
 const computerScore = 0; 
 
-let computerChoice = document.getElementById("computerChoice");
-let option = document.getElementById("choicebutton")
-
-
-function createComDecision(){
+function createComSelection(){
     const randomChoice = Math.Floor(Math.random() * 3);
     return choicebutton[randomChoice];
-}
-
-computerChoice = `Computers Choice ${createComDecision()}`;
-
-function play(playerSelection){
- let playerChoice = document.getElementById("playerChoice");
- playerChoice.innerHTML = `You Selected: $(playerChoice)`;
-
- returnResult(playerSelection);
-}
-
-const displayScoreResults = () => {
-    let playerScoreResults = document.getElementById("playerScore");
-    playerScoreResults.innerHTML = playerScore;
-    let computerScoreResults = document.getElementById("computerScore");
-    computerScoreResults.innerHTML = computerScore;
 }
 
 function play(computerSelection){
@@ -36,6 +16,14 @@ function play(computerSelection){
    
     returnResult(computerSelection);
    }
+const displayScoreResults = () => {
+    let playerScoreResults = document.getElementById("playerScore");
+    playerScoreResults.innerHTML = playerScore;
+    let computerScoreResults = document.getElementById("computerScore");
+    computerScoreResults.innerHTML = computerScore;
+}
+
+
 
 function returnResult(playerChoice, computerChoice) {
     if (playerChoice === computerChoice){
