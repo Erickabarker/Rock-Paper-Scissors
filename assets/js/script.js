597ae4f006c01dbf1b02bdef71a57d67f6f14255
Erickabarker <110427318+Erickabarker@users.noreply.github.com>
2022-12-06 19:22:50 +0000

    added comments wiht updated returnResult function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,59 +4,70 @@ const scissors = document.getElementById("scissors");
 const choicebutton = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
+let winner;
 
 // function creates computer random selection
-function createComSelection(){
+function createComSelection() {
     let randomChoice = choicebutton[Math.floor(Math.random() * 3)]
     console.log(randomChoice, playerChoice)
     computerChoice.innerHTML = randomChoice;
-    console.log("createComSelection:", randomChoice, computerChoice)
     return randomChoice
 }
 
-function displayScoreResults(){
-   let playerScoreResults = document.getElementById("playerScore");
-    playerScoreResults.innerHTML = playerScore++;
+function displayScoreResults(winner) {
+    let playerScoreResults = document.getElementById("playerScore");
     let computerScoreResults = document.getElementById("computerScore");
-    computerScoreResults.innerHTML = computerScore++;
-    console.log("displayScoreResults");
+    if (winner = "player") {
+        playerScoreResults.innerHTML = playerScore++;
+    } else {
+        computerScoreResults.innerHTML = computerScore++;
+    }
+    console.log("displayScoreResults", winner)
 }
+
 //Function checks whether player or computer will win
 
 function returnResult(playerChoice, computerHTML) {
     (computerHTML == "rock")
-if (playerChoice == "paper"){
-returnResult.innerHTML = "You Win! Let's go Again"
-} else if (playerChoice == "rock"){
-    returnResult = "It's a draw, Let's Try Again!"
-} else if (playerChoice == "scissors"){
-    returnResult.innerHTML = "You Lost! Let's Try Again!"
-}
+    if (playerChoice == "paper") {
+        returnResult.innerHTML = "You Win! Let's go Again"
+        winner = "player";
+    } else if (playerChoice == "rock") {
+        returnResult = "It's a draw, Let's Try Again!"
+    } else if (playerChoice == "scissors") {
+        returnResult.innerHTML = "You Lost! Let's Try Again!"
+        winner = "computer";
+    }
     (computerHTML == "paper")
-if (playerChoice == "rock"){
-returnResult.innerHTML = "You Lost! Let's Try Again!"
-} else if (playerChoice == "paper"){
-    returnResult = "It's a draw, Let's Try Again!"
-} else if (playerChoice == "scissors"){
-    returnResult.innerHTML = "You Win! Let's go Again"
-}
-    (computerHTML == "scissors")
-if (playerChoice == "rock"){
-returnResult.innerHTML = "You Win! Let's go Again"
-} else if (playerChoice == "paper"){
-    returnResult = "You Lost! Let's Try Again!"
-} else if (playerChoice == "scissors"){
-    returnResult.innerHTML = "It's a draw, Let's Try Again!"
-}
+    if (playerChoice == "rock") {
+        returnResult.innerHTML = "You Lost! Let's Try Again!"
+        winner = "computer";
+    } else if (playerChoice == "paper") {
+        returnResult = "It's a draw, Let's Try Again!"
+    } else if (playerChoice == "scissors") {
+        returnResult.innerHTML = "You Win! Let's go Again"
+        winner = "player";
+    }
+        (computerHTML == "scissors")
+    if (playerChoice == "rock") {
+        returnResult.innerHTML = "You Win! Let's go Again"
+        winner = "player";
+    } else if (playerChoice == "paper") {
+        returnResult = "You Lost! Let's Try Again!"
+        winner = "computer";
+    } else if (playerChoice == "scissors") {
+        returnResult.innerHTML = "It's a draw, Let's Try Again!"
+    }
 
-displayScoreResults();
-console.log(playerScore, computerScore);
+
+    displayScoreResults(winner);
+    console.log(playerScore, computerScore, winner)
 }
 
 /*Event listeners for user choice input
 * createComSelection and returnResult is called here
 */
-scissors.addEventListener("click", function (){
+scissors.addEventListener("click", function () {
     let playerHTML = document.getElementById("playerChoice");
     playerHTML.innerHTML = "You chose scissors!"
     playerChoice = "scissors"
@@ -68,7 +79,7 @@ scissors.addEventListener("click", function (){
 })
 
 
-rock.addEventListener("click", function (){
+rock.addEventListener("click", function () {
     let playerHTML = document.getElementById("playerChoice");
     playerHTML.innerHTML = "You chose Rock!"
     playerChoice = "rock"
@@ -81,7 +92,7 @@ rock.addEventListener("click", function (){
     returnResult();
 })
 
-paper.addEventListener("click", function (){
+paper.addEventListener("click", function() {
     let playerHTML = document.getElementById("playerChoice");
     playerHTML.innerHTML = "You chose Paper!"
     playerChoice = "paper"
@@ -95,13 +106,24 @@ paper.addEventListener("click", function (){
 })
 
 //Game reset button input to restart game
-document.getElementsByClassName("gameResetButton").reset; {
-    gameReset();
- };
-
-gameReset.addEventListener("button", function gameReset() {
+function gameReset() {
     playerScore.innerText = 0;
     computerScore.innerText = 0;
-    returnResult.innerHTML = ("");
-    document.getElementsByClassName("gameResetButton");
-})
+    returnResult.innerHTML = "Game Restarted";
+    document.getElementById("gameReset");
+
+console.log("gameReset");
+}
+
+gameResetButton.addEventListener("click", function() {
+    let gameReset = document.getElementById("gameReset");
+    gameReset.innerHTML = "Game Reset"
+
+    gameReset();
+});
+
+document.getElementById("gameReset").addEventListener("click", function() {
+    document.getElementById("gameResetContainer").classList.add("hide");
+gameReset();
+ });
+
